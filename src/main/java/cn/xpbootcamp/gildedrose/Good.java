d139ae93a4e9a6e7313534a05f5bd99ed6f7a36c
Yanmin XING <yanmin@Yanmins-MacBook-Pro.local>
2020-08-24 23:44:43 +0800

    limit good quality value max when new

--- src/main/java/cn/xpbootcamp/gildedrose/Good.java
+++ src/main/java/cn/xpbootcamp/gildedrose/Good.java
@@ -1,11 +1,18 @@
 package cn.xpbootcamp.gildedrose;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+        import lombok.Getter;
 
-@AllArgsConstructor
 @Getter
 public class Good {
     private int sellIn;
     private int quality;
+    private int QUALITY_MAX_LIMIT = 50;
+
+    public Good(int sellIn, int quality) {
+
+        if (quality > QUALITY_MAX_LIMIT) throw new IllegalArgumentException("illegal quality value");
+        this.sellIn = sellIn;
+        this.quality = quality;
+    }
+
 }
---
+++ src/test/java/cn/xpbootcamp/gildedrose/GoodTest.java
@@ -0,0 +1,20 @@
+package cn.xpbootcamp.gildedrose;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class GoodTest {
+
+    @Test
+    public void should_throw_exception_when_new_good_given_quality_greater_than_max_limit() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Good(10, 60);
+        });
+
+        String expectedMessage = "illegal quality value";
+        String actualMessage = exception.getMessage();
+
+        assertEquals(expectedMessage, actualMessage);
+    }
+}
