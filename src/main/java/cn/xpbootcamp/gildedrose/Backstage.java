690f654b1cad9379afa906016972ee2b007713f0
Yanmin XING <yanmin@Yanmins-MacBook-Pro.local>
2020-08-25 06:28:09 +0800

    backstage quality should add 1 each day before ten days

--- src/main/java/cn/xpbootcamp/gildedrose/Backstage.java
+++ src/main/java/cn/xpbootcamp/gildedrose/Backstage.java
@@ -4,7 +4,18 @@ import lombok.Getter;
 
 @Getter
 public class Backstage extends Good {
+
+    private final int EACH_DAY_INCREASED_QUALITY_TEN_DAY_BEFORE = 1;
+
     public Backstage(int sellIn, int quality) {
         super(sellIn, quality);
+    }
+
+    public int getUpdatedSellIn(int day) {
+        return this.sellIn - day;
+    }
+
+    public int getUpdatedQuality(int day) {
+        return this.quality + day * EACH_DAY_INCREASED_QUALITY_TEN_DAY_BEFORE;
     }
 }
--- src/test/java/cn/xpbootcamp/gildedrose/BackstageTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/BackstageTest.java
@@ -13,4 +13,13 @@ public class BackstageTest {
         Assert.assertEquals(10, backstage.getSellIn());
         Assert.assertEquals(20, backstage.getQuality());
     }
+
+    @Test
+    public void should_increase_one_when_get_good_updatedQuality_and_updatedSelIn_given_one_day_passed_and_updatedSellIn_greater_than_ten() {
+
+        Backstage backstage = new Backstage(15, 20);
+
+        Assert.assertEquals(14, backstage.getUpdatedSellIn(1));
+        Assert.assertEquals(21, backstage.getUpdatedQuality(1));
+    }
 }
