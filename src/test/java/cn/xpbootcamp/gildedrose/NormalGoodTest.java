218dd926d55e0be6d20a3991d61f63cb77c610d4
Yanmin XING <yanmin@Yanmins-MacBook-Pro.local>
2020-08-25 06:53:11 +0800

    add nomal good test case which in ac

--- src/test/java/cn/xpbootcamp/gildedrose/NormalGoodTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/NormalGoodTest.java
@@ -17,10 +17,14 @@ public class NormalGoodTest {
     @Test
     public void should_reduce_one_when_get_good_updatedQuality_and_updatedSelIn_given_one_day_passed() {
 
-        NormalGood normalGood = new NormalGood(10, 20);
+        NormalGood normalGood1 = new NormalGood(10, 20);
+        NormalGood normalGood2 = new NormalGood(3, 6);
 
-        Assert.assertEquals(9, normalGood.getUpdatedSellIn(1));
-        Assert.assertEquals(19, normalGood.getUpdatedQuality(1));
+        Assert.assertEquals(9, normalGood1.getUpdatedSellIn(1));
+        Assert.assertEquals(19, normalGood1.getUpdatedQuality(1));
+
+        Assert.assertEquals(2, normalGood2.getUpdatedSellIn(1));
+        Assert.assertEquals(5, normalGood2.getUpdatedQuality(1));
     }
 
     @Test
